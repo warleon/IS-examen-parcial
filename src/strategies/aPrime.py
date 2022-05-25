2796585113dfbd36fa4907918f87d78e5e5db333
warleon <51892390+warleon@users.noreply.github.com>
2022-05-24 21:24:14 -0500

    finish aPrime strategy

--- src/strategies/aPrime.py
+++ src/strategies/aPrime.py
@@ -1,7 +1,17 @@
 class APrime():
 	def __init__(self):
 		self.count=0
-		self.isPrime=[True for i in range(1,101)]
+		self.isPrime= self.create_primes()
+
+	def create_primes(self):
+		isPrime=[True for i in range(0,101)]
+		isPrime[0]=False
+		isPrime[1]=False
+		for i in range(2,101):
+			if isPrime[i]:
+				for j in range(2*i,101,i):
+					isPrime[j]=False
+		return isPrime
 
 	def check_win(self,number):
 		if(self.isPrime[number]):
